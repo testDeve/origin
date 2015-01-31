52e11079703979aef33fda93a076e7e7d979a0ad
testDeve <owner@192.168.11.6>
2015-01-31 14:23:00 +0900

    コミット

---
+++ DesignPattern/src/Adapter/BannerTest.java
@@ -0,0 +1,21 @@
+package Adapter;
+
+import AbstractFactory.Main;
+import junit.framework.TestCase;
+
+public class BannerTest extends TestCase{
+    
+    public void testBanner(){
+         Banner banner = new Banner("Banner");   
+	}
+    
+    public void testShowWithParen(){
+         Banner banner = new Banner("Banner");
+         banner.showWithParen();
+	}
+    
+    public void testShowWithAster(){
+        Banner banner = new Banner("Banner");
+        banner.showWithAster();
+   }
+}
---
+++ DesignPattern/src/ChainOfResponsibility/Trouble.java
@@ -0,0 +1,16 @@
+package ChainOfResponsibility;
+
+public class Trouble {
+	private int number;
+	public Trouble(int number){
+		this.number = number;
+	}
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public String toString(){
+		return "[Trouble " + number + "]";
+	}
+}
