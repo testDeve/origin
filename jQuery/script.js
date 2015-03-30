1dcf11e55c631566c6c62386aaf3ee99f70b208b
村舘　孝哉 <takaya.muradate@nttd-wave.com>
2015-03-30 17:52:31 +0900

    Changes to be committed: 	modified:   script.js

--- jQuery/script.js
+++ jQuery/script.js
@@ -8,3 +8,50 @@ function json(){
         }
     }
 }
+
+$(function(){
+    $(".hoge1").css("color","blue")
+});
+
+$(function(){
+  $(".hoge2A").click(function(){
+      $(this).css("background-color","green");
+  })
+});
+  
+$(function(){
+  $(".hoge2B").click(function(){
+      $(".hoge2B").toggle();
+  });
+});
+
+$(function(){
+  $(".hoge2C").click(function(){
+      $(this).hide();
+  });
+});
+
+
+function fadeOut(){
+  $(".hoge2D").fadeOut(20000);
+}
+
+$(function() {
+   $("#tbl2 tr:odd td").css('background', 'rgb(198, 226, 255)');
+});
+
+$(function() {
+  $('#navi > h4').click(function(){
+      $(this).next().slideToggle(100);
+  });
+});
+
+$("#animate").click(function(){
+  $(".block").animate(
+    {width: "toggle", opacity: "toggle"},
+    {duration: "slow", easing: "linear",
+     complete: function(){alert("completed!");},
+     step: function(s){$("#steps").text(s)}
+    }
+  );
+});
